ac0228070fc30059013195a216ca5880673da115
Vitaly Baev <dj@vitalybaev.ru>
2016-08-21 15:14:15 +0300

    Исправление ошибок

--- src/Tret/Text.php
+++ src/Tret/Text.php
@@ -40,7 +40,7 @@ class Text extends Base
 					),
 				'replacement' 	=> array(
 					'\1',
-					'$m[1].($m[7] === Lib::strtolower($m[3]) ? $m[2] : $m[2].$m[5].$m[6] )',
+					'$m[1].($m[7] === Emuravjev\Mdash\Lib::strtolower($m[3]) ? $m[2] : $m[2].$m[5].$m[6] )',
 					)
 			),
 		'paragraphs' => array(
@@ -65,7 +65,7 @@ class Text extends Base
 		$text = '<' . self::BASE64_PARAGRAPH_TAG . '>' . trim($text) . '</' . self::BASE64_PARAGRAPH_TAG . '>';
 		//$text = $this->preg_replace_e('/([\040\t]+)?(\n|\r){2,}/e', '"</" . self::BASE64_PARAGRAPH_TAG . "><" .self::BASE64_PARAGRAPH_TAG . ">"', $text);
 		//$text = $this->preg_replace_e('/([\040\t]+)?(\n){2,}/e', '"</" . self::BASE64_PARAGRAPH_TAG . "><" .self::BASE64_PARAGRAPH_TAG . ">"', $text);
-		$text = $this->preg_replace_e('/([\040\t]+)?(\n)+([\040\t]*)(\n)+/e', '$m[1]."</" . self::BASE64_PARAGRAPH_TAG . ">".Lib::iblock($m[2].$m[3])."<" .self::BASE64_PARAGRAPH_TAG . ">"', $text);
+		$text = $this->preg_replace_e('/([\040\t]+)?(\n)+([\040\t]*)(\n)+/e', '$m[1]."</" . self::BASE64_PARAGRAPH_TAG . ">".Emuravjev\Mdash\Lib::iblock($m[2].$m[3])."<" .self::BASE64_PARAGRAPH_TAG . ">"', $text);
 		//$text = $this->preg_replace_e('/([\040\t]+)?(\n)+([\040\t]*)(\n)+/e', '"</" . self::BASE64_PARAGRAPH_TAG . ">"."<" .self::BASE64_PARAGRAPH_TAG . ">"', $text);
 		//может от открвающего до закрывающего ?!
 		$text = preg_replace('/\<' . self::BASE64_PARAGRAPH_TAG . '\>('.Lib::INTERNAL_BLOCK_OPEN.'[a-zA-Z0-9\/=]+?'.Lib::INTERNAL_BLOCK_CLOSE.')?\<\/' . self::BASE64_PARAGRAPH_TAG . '\>/s', "", $text);
@@ -101,7 +101,7 @@ class Text extends Base
 	 */
 	protected function build_brs()
 	{
-		$this->_text = $this->preg_replace_e('/(\<\/' . self::BASE64_PARAGRAPH_TAG . '\>)([\r\n \t]+)(\<' . self::BASE64_PARAGRAPH_TAG . '\>)/mse', '$m[1].Lib::iblock($m[2]).$m[3]', $this->_text);
+		$this->_text = $this->preg_replace_e('/(\<\/' . self::BASE64_PARAGRAPH_TAG . '\>)([\r\n \t]+)(\<' . self::BASE64_PARAGRAPH_TAG . '\>)/mse', '$m[1].Emuravjev\Mdash\Lib::iblock($m[2]).$m[3]', $this->_text);
 
 		if (!preg_match('/\<' . self::BASE64_BREAKLINE_TAG . '\>/', $this->_text)) {
 			$this->_text = str_replace("\r\n","\n",$this->_text);
@@ -111,36 +111,4 @@ class Text extends Base
 		}
 	}
 }
-/**PYTHON
-
-    def do_paragraphs(self, text):
-        text = Lib.str_replace(u"\r\n",u"\n",text)
-        text = Lib.str_replace(u"\r",u"\n",text)
-        text = u'<' + BASE64_PARAGRAPH_TAG + u'>' + text.strip() + u'</' + BASE64_PARAGRAPH_TAG + u'>'
-        text = self.preg_replace('/([\040\t]+)?(\n)+([\040\t]*)(\n)+/e', '(u"" if m.group(1) is None else m.group(1))+u"</" + BASE64_PARAGRAPH_TAG + u">"+Lib.iblock(m.group(2)+m.group(3))+u"<" +BASE64_PARAGRAPH_TAG + u">"', text)
-        text = self.preg_replace('/\<' + BASE64_PARAGRAPH_TAG + '\>(' + INTERNAL_BLOCK_OPEN + '[a-zA-Z0-9\/=]+?' + INTERNAL_BLOCK_CLOSE + ')?\<\/' + BASE64_PARAGRAPH_TAG + '\>/s', "", text)
-        return text
-
-
-    def build_paragraphs(self):
-        r = self._text.find( u'<' + BASE64_PARAGRAPH_TAG + u'>' )
-        p = self._text.rfind( u'</'+  BASE64_PARAGRAPH_TAG + u'>' )
-        if(( r != -1) and (p != -1)):
-
-            beg = Lib.substr(self._text,0,r);
-            end = Lib.substr(self._text,p+len(u'</' + BASE64_PARAGRAPH_TAG + u'>'))
-            self._text = (self.do_paragraphs(beg)+ u"\n" if beg.strip() else u"") +u'<' + BASE64_PARAGRAPH_TAG + u'>'+Lib.substr(self._text,r + len(u'<' + BASE64_PARAGRAPH_TAG + u'>'),p -(r + len(u'<' + BASE64_PARAGRAPH_TAG + u'>')) )+u'</' + BASE64_PARAGRAPH_TAG + u'>'+( u"\n"+self.do_paragraphs(end) if end.strip() else u"")
-        else:
-            self._text = self.do_paragraphs(self._text)
-
-    def build_brs(self):
-        self._text = self.preg_replace('/(\<\/' + BASE64_PARAGRAPH_TAG + '\>)([\r\n \t]+)(\<' + BASE64_PARAGRAPH_TAG + '\>)/mse', 'm.group(1)+Lib.iblock(m.group(2))+m.group(3)', self._text);
-
-        if (not re.match('\<' + BASE64_BREAKLINE_TAG + '\>', self._text)):
-            self._text = Lib.str_replace("\r\n","\n",self._text)
-            self._text = Lib.str_replace("\r","\n",self._text)
-            self._text = self.preg_replace('/(\n)/e', '"<" + BASE64_BREAKLINE_TAG + ">\\n"', self._text)
-
-
-PYTHON**/
 ?>
